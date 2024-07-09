aa6210fc230ac3869dccc48850a2877aba255134
zententakuto <167833234+zententakuto@users.noreply.github.com>
2024-07-09 09:53:22 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,3 +1,13 @@
 alert("Hello,Takuto!");
 alert(1+1);
 alert(Math.sqrt(1+1));
+
+alert(Math.sqrt(2+3));
+alert(1/0);
+alert(0/0);
+
+alert(Math.sqrt(2+3));
+alert(Math.sqrt("2"+"3"));
+alert(Math.sqrt(Number("2")+("3")));
+alert(Math.sqrt("2"+3));
+alert(Math.sqrt(Number("2")+3));
